d4ec3ac7d5f5274580384bc41101b6c211e81b88
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-11-24 21:05:47 +0300

    Update views.py
    updated views

--- christmass/home/views.py
+++ christmass/home/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def message(request):
+    if request.method == 'POST':
+        sender = request.POST['name']
+        if sender is not None:
+            user = User.objects.create_user(username=sender)
+            user.save()
+            return render(request,'message.html')
+    else:
+        return render(request,'index.html')
