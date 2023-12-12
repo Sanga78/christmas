fdf74953b5ad414ca3955b6e565bd39d028b1672
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2023-12-12 10:12:22 +0300

    Update models.py
    updated the models

--- christmass/home/models.py
+++ christmass/home/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import CustomUserManager
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add your additional fields if needed
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
